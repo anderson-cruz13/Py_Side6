b49af4d9f9d3659f05b29511dc1688bdbd1da137
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-26 10:51:08 -0300

    test: testando um label

--- Py_Calculadora/main.py
+++ Py_Calculadora/main.py
@@ -1,9 +1,14 @@
 import sys
-from main_window import MainWindow
-from PySide6.QtWidgets import QApplication
+from main_window import MainWindow  # type: ignore
+from PySide6.QtWidgets import QApplication, QLabel
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    label1 = QLabel('Teste')
+    label1.setStyleSheet('font-size: 50px')
+    window.v_layout.addWidget(label1)
+
     window.show()
     app.exec()
