79e17b950c2f0804a0e694eb3ad97e6fb69d9cd5
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-26 10:45:53 -0300

    refactor: extraindo main para novo arquivo.

--- Py_Calculadora/main.py
+++ Py_Calculadora/main.py
@@ -1,15 +1,9 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from main_window import MainWindow
+from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    window = QMainWindow()
-    cw = QWidget()
-    v_layout = QVBoxLayout()
-    cw.setLayout(v_layout)
-
-    window.setCentralWidget(cw)
+    window = MainWindow()
     window.show()
-
     app.exec()
