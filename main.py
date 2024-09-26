992c0976a3cafa6aac2a78b3fab9ebc65e86a27d
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-26 07:50:00 -0300

    feat: :art: Colocando botões em um widget com padrão de column

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import sys
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QVBoxLayout,
+    QWidget)
 
 app = QApplication(sys.argv)
 
 botao = QPushButton("Texto do botão")
-botao.show()
+botao.setStyleSheet('font-size: 80px')
+botao.show()  # Renderizar bottom
+botao2 = QPushButton("Texto do botão 2")
+botao2.setStyleSheet('font-size: 40px')
+botao2.show()
 
+central_widget = QWidget()
+layout = QVBoxLayout()  # Column
+central_widget.setLayout(layout)
+layout.addWidget(botao)
+layout.addWidget(botao2)
+
+central_widget.show()
 app.exec()
