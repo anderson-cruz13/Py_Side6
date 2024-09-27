ab91f134629cec690d0823aa2496483316883d86
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-27 10:08:27 -0300

    feat: add icon na aplicação

--- Py_Calculadora/main.py
+++ Py_Calculadora/main.py
@@ -1,6 +1,8 @@
 import sys
 from main_window import MainWindow  # type: ignore
 from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QIcon
+from variables import WINDOW_ICON_PATH
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -10,6 +12,10 @@ if __name__ == "__main__":
     label1.setStyleSheet('font-size: 50px')
     window.addWidgetToVLayout(label1)
     window.adjustFixedSize()
+
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
 
     window.show()
     app.exec()
