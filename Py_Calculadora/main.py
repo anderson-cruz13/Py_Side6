da83b0ab37a05df1459939642b725497718f6d0e
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-28 08:36:55 -0300

    test: testando esqueleto da grid para os botões da calculadora

--- Py_Calculadora/main.py
+++ Py_Calculadora/main.py
@@ -4,7 +4,7 @@ from variables import WINDOW_ICON_PATH
 from display import Display
 from info import Info
 from style import setupTheme
-from buttons import Button
+from buttons import Button, ButtonsGrid
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 
@@ -22,15 +22,29 @@ if __name__ == "__main__":
 
     # Info
     info = Info(text="teste")
-    window.addToVLayout(info)
+    window.addWidgetToVLayout(info)
 
     # Displat
     display = Display()
-    window.addToVLayout(display)
+    window.addWidgetToVLayout(display)
 
-    # Button
-    button = Button("Botão")
-    window.addToVLayout(button)
+    # Grid
+    buttonsGrid = ButtonsGrid()
+    window.vLayout.addLayout(buttonsGrid)
+
+    # Botão
+    buttonsGrid.addWidget(Button("9"), 0, 2)
+    buttonsGrid.addWidget(Button("8"), 0, 1)
+    buttonsGrid.addWidget(Button("7"), 0, 0)
+    buttonsGrid.addWidget(Button("6"), 1, 2)
+    buttonsGrid.addWidget(Button("5"), 1, 1)
+    buttonsGrid.addWidget(Button("4"), 1, 0)
+    buttonsGrid.addWidget(Button("3"), 2, 2)
+    buttonsGrid.addWidget(Button("2"), 2, 1)
+    buttonsGrid.addWidget(Button("1"), 2, 0)
+    buttonsGrid.addWidget(Button("."), 3, 2)
+    buttonsGrid.addWidget(Button("0"), 3, 1)
+    buttonsGrid.addWidget(Button("+/-"), 3, 0)
 
     # Loop de execução
     window.adjustFixedSize()
