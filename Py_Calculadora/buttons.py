7cfbf33e168ff1d699b66c7fe81a9745df1092d1
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-28 10:09:06 -0300

    feat: add interação dos botões com o display

--- Py_Calculadora/buttons.py
+++ Py_Calculadora/buttons.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
+from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE
 from utils import isNumOrDot
+from display import Display
 
 
 class Button(QPushButton):
@@ -8,15 +10,16 @@ class Button(QPushButton):
         super().__init__(*args, **kwargs)
         self.configStyle()
 
-    def configStyle(self):
+    def configStyle(self) -> None:
         font = self.font()
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
         self.setMinimumSize(75, 75)
+        self.setCheckable(True)
 
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, display: Display, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -26,10 +29,10 @@ class ButtonsGrid(QGridLayout):
             ['1', '2', '3', '+'],
             ['', '0', '.', '=']
         ]
-
+        self.display = display
         self._makeGrid()
 
-    def _makeGrid(self):
+    def _makeGrid(self) -> None:
         for i, row in enumerate(self._gridMask):
             for j, buttonText in enumerate(row):
                 button = Button(buttonText)
@@ -41,3 +44,19 @@ class ButtonsGrid(QGridLayout):
                     button.setProperty('cssClass', 'specialButton')
 
                 self.addWidget(button, i, j)
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button,
+                )
+
+                button.clicked.connect(buttonSlot)
+
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextToDisplay(self, button) -> None:
+        buttonText = button.text()
+        self.display.insert(buttonText)
