d15714579ac8c6fb6cc4438f028e2b8514199f23
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-28 08:50:17 -0300

    feat: criando botões dinâmicamente a partir do método makeGrid

--- Py_Calculadora/buttons.py
+++ Py_Calculadora/buttons.py
@@ -18,10 +18,20 @@ class ButtonsGrid(QGridLayout):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._grid_mask = [
+        self._gridMask = [
             ['C', '<', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
             ['', '0', '.', '=']
         ]
+
+        self._makeGrid()
+
+    def _makeGrid(self):
+        for i, row in enumerate(self._gridMask):
+            for j, buttonText in enumerate(row):
+                if buttonText == '':
+                    continue
+                else:
+                    self.addWidget(Button(buttonText), i, j)
