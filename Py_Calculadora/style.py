352767c67caed3903dac7058472ab165bb29c18d
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-28 08:18:37 -0300

    short: correação da propriedade css do QPushButton

--- Py_Calculadora/style.py
+++ Py_Calculadora/style.py
@@ -6,16 +6,16 @@ from variables import (
 
 
 qss = f"""
-    PushButton[cssClass="specialButton"] {{
+    QPushButton[cssClass="specialButton"] {{
         color: #fff;
         background: {PRIMARY_COLOR};
         border-radius: 5px;
     }}
-    PushButton[cssClass="specialButton"]:hover {{
+    QPushButton[cssClass="specialButton"]:hover {{
         color: #fff;
         background: {DARKER_PRIMARY_COLOR};
     }}
-    PushButton[cssClass="specialButton"]:pressed {{
+    QPushButton[cssClass="specialButton"]:pressed {{
         color: #fff;
         background: {DARKEST_PRIMARY_COLOR};
     }}
